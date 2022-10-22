32b54919b08b16288a3fdf73c682fa34960e7e53
Arthur Dev <73297053+ArthurDeveloper@users.noreply.github.com>
2022-10-22 20:47:13 -0300

    feat: Now the bot can read messages

--- app/src/main/java/awesome/bot/Bot.java
+++ app/src/main/java/awesome/bot/Bot.java
@@ -1,7 +1,11 @@
 package awesome.bot;
 
-import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import javax.security.auth.login.LoginException;
 
@@ -9,16 +13,28 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class Bot {
+public class Bot extends ListenerAdapter {
     public static void main(String[] args) throws LoginException, IOException {
-		Path tokenFilePath = Path.of(".\\token.txt");
+		Path tokenFilePath = Path.of("token.txt");
 		String token = Files.readString(tokenFilePath);
 		token = token.replaceAll("\\x0d", " ").trim();
 
 		JDABuilder builder = JDABuilder.createDefault(token);
+		builder.enableIntents(GatewayIntent.MESSAGE_CONTENT);
 
-		builder.build();
+		JDA jda = builder.build();
+		jda.addEventListener(new Bot());
 
 		System.out.println("Bot's working!");
     }
+
+	@Override
+	public void onReady(ReadyEvent ready) {
+		System.out.println("Ready!");
+	}
+
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event) {
+		System.out.printf("%s sent: %s\n", event.getAuthor().getName(), event.getMessage().getContentDisplay());
+	}
 }
